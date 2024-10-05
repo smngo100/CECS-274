b328de00ed39a1fdd02f0691243e273a0baaff0a
smngo100 <smngo100@gmail.com>
2024-10-05 16:22:06 -0700

    Add files via upload

---
+++ project_template/algorithms.py
@@ -0,0 +1,21 @@
+"""Implementations of some sorting"""
+import random
+
+
+def merge(a0, a1, a):
+    # todo
+    pass   
+
+def merge_sort(a):
+    # todo
+    pass   
+
+
+def _quick_sort(a, i, n):
+    # todo
+    pass   
+
+
+def quick_sort(a):
+    _quick_sort(a, 0, len(a))
+    
