15b01dac4f4701820617823d632b5bc1db6cdb40
smngo100 <smngo100@gmail.com>
2024-12-01 17:19:10 -0800

    Update algorithms.py

--- Module 07/algorithms.py
+++ Module 07/algorithms.py
@@ -15,21 +15,6 @@ def _merge(leftArray, rightArray, array):
 		else: 
 			a[i] = rightArray[j]
 			j += 1 
-			
-        
-# Version 2 of _merge helper function 
-def _merge(leftArray, rightArray, array):
-	i = 0 # left tracking position
-	j = 0 # right tracking position  
-	n = len(array) # number of elements in the array
-	
-	while i < len(leftArray) and j < len(rightArray): 
-		if leftArray[i] < rightArray[j]:
-			result.append(left[i])
-			i += 1 
-		else:
-			result.append(right[j]
-			j += 1 
 
 
 def Merge_sort(array):
@@ -43,8 +28,79 @@ def Merge_sort(array):
 	sorted_left_half = merge_sort(leftHalf)
 	sorted_right_half = merge_sort(rightHalf)
 	
-	return _merga(sorted_left_half, sorted_right_half, array)
+	return _merge(sorted_left_half, sorted_right_half, array)
 	
 
-    
+
+
+
+
+
+
+
+
+
+def _partition_f(a: List, start, end):
+	pivot = a[start]
+	i = start + 1 
+	j = end - 1
+
+	while i < j: 
+		while i < end and a[i] < pivot: 
+			i += 1 
+		while j > start and a[j] >= pivot: 
+			j -= 1 
+		if i < j: 
+			a[i], a[j], a[j], a[i]
+
+	if a[j] > pivot: 
+		a[j], a[end] = a[end], a[j]
+
+	return i 
+
+
+def _quick_sort_f(a: List, start, end):
+	if start < end:
+        	p = _partition_f(a, start, end)
+
+        	# Recursion
+        	_quick_sort_f(a, start, p - 1)  # first half
+        	_quick_sort_f(a, p + 1, end)    # second half
+    	elif start >= end:
+        	return a
+
+
+def _quick_sort_r(a: List, start, end):
+	if start < end:
+        	p = _partition_f(a, start, end)
+
+        	# Recursion
+        	_quick_sort_f(a, start, p - 1)  # first half
+        	_quick_sort_f(a, p + 1, end)    # second half
+    	elif start >= end:
+        	return a
+
+
+def quick_sort(a: List, p=True):
+	"""
+	sorts an ArrayList a using the quick sort algorithm.
+	If parameter p is True, the quick sort algorithm uses a randomly chosen element from a as pivot.
+	Otherwise, the quick sort algorithm uses the first element as pivot.
+	"""
+	if p:
+		_quick_sort_r(a, 0, len(a) - 1) 
+	else:
+		_quick_sort_f(a, 0, len(a) - 1) 
+	
+
+
+
+
+
+
+
+
+
+
+
 
