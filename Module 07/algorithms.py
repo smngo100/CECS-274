1b094f5785528cbc620c0b57f09b7f8d3857869c
smngo100 <smngo100@gmail.com>
2024-12-01 21:13:11 -0800

    Update algorithms.py

--- Module 07/algorithms.py
+++ Module 07/algorithms.py
@@ -5,10 +5,10 @@ from Interfaces import List
 
 
 def linear_search(a: List, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return None
+    for i in range(len(a)):     # for every element in the array a
+        if a[i] == x:           # if the element at index i in the array a equals the value we're searching for (x)
+            return i            # return that element
+    return None                 # else, return None
 
 
 def binary_search(a: List, x):
@@ -16,30 +16,30 @@ def binary_search(a: List, x):
     r = len(a) - 1      # right bound
     while l <= r:
         m = (l + r) // 2
-        if x == a[m]:
-            return m
-        elif x < a[m]:
-            a[0:m]
-            r = m - 1
+        if x == a[m]:   # if the value we're searching for (x) equals the element at index m in the array a
+            return m        # we return m
+        elif x < a[m]:  # else, if the value we are searching for (x) is less than the element at index m in the array a
+            a[0:m]          # that means x is in the first half of the array
+            r = m - 1       # we'll look at all the values to the left of it
         else:
-            a[m:r]
-            l = m + 1
+            a[m:r]          # else, x is in the second half of the array
+            l = m + 1       # we'll look at all the values to the right of it
     return None
 
 
 def _merge(a0: List, a1: List, a: List):
-    i0 = 0
-    i1 = 0
-    n = len(a)
+    i0 = 0      # tracking position of the first subarray
+    i1 = 0      # tracking position of the second subarray
+    n = len(a)  # number of elements in the array
 
-    for i in range(n):
-        if i0 >= len(a0):
-            a[i] = a1[i1]
-            i1 += 1
-        elif i1 >= len(a1):
-            a[i] = a0[i0]
-            i0 += 1
-        elif a0[i0] <= a1[i1]:
+    for i in range(n):      # for every element in the array
+        if i0 >= len(a0):       # if the tracking position in the 1st subarray (i0) >= the len of the 1st subarray
+            a[i] = a1[i1]           # the element at index i in the array a = the element i1 in the 2nd subarray
+            i1 += 1                 # increment the tracking position of the 2nd subarray
+        elif i1 >= len(a1):     # else, if tracking position of 2nd subarray >= len of 2nd subarray
+            a[i] = a0[i0]           # the element at index i in the array = the element i0 in the 1st subarray
+            i0 += 1                 # increment the tracking position of the 1st subarray
+        elif a0[i0] <= a1[i1]:  
             a[i] = a0[i0]
             i0 += 1
         else:
@@ -53,9 +53,9 @@ def merge_sort(a: List):
     if n == 0 or n == 1:
         return a
 
-    m = n // 2       # mid
-    a0 = a[0:m]       # first half of the array
-    a1 = a[m:n]       # second half of the array
+    m = n // 2      # mid
+    a0 = a[0:m]     # first half of the array
+    a1 = a[m:n]     # second half of the array
 
     # Recursively sort a0 and a1
     merge_sort(a0)
@@ -146,6 +146,8 @@ def quick_sort(a: List, p=True):
     Otherwise, the quick sort algorithm uses the first element as pivot.
     """
     if p:
+        #a.a.sort()
         _quick_sort_r(a, 0, len(a) - 1)
     else:
+        #a.a.sort()
         _quick_sort_f(a, 0, len(a) - 1)
