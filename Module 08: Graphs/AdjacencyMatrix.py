5454543f27c56518b7dd222d33ba95e6083a09db
smngo100 <smngo100@gmail.com>
2024-12-03 11:37:37 -0800

    Update AdjacencyMatrix.py

--- Module 08: Graphs/AdjacencyMatrix.py
+++ Module 08: Graphs/AdjacencyMatrix.py
@@ -8,31 +8,46 @@ class AdjacencyMatrix(Graph):
         self.n = n
         self.adj = np.zeros(n*n)
 
+    
     def add_edge(self, i : int, j : int):
         a[i][j] = True    # add the edge (i, j) to E   
 
+    
     def remove_edge(self, i : int, j : int):
-        a[i][j] = False    # remove the edge (i, j) from E 
+        if a[i][j] == 1: 
+            a[i][j] = 0
+            return True 
+        return False
+        #a[i][j] = False    # remove the edge (i, j) from E 
+    
 
     def has_edge(self, i : int, j: int) ->bool:
         return a[i][j]    # returns true if the edje (i, j) exists in E 
 
+    
     def out_edges(self, i) -> List:
         # todo        # returns a list of all integers j such that (i, j) ∈ E
-        pass
+        for i in j:
+            if a[i][j] == 1: 
+                edges.append[j]
+        return edges 
 
+    
     def in_edges(self, i) -> List:
         # todo         # returns a list of all integers i such that (i, j) ∈ E
         pass
 
+    
     def bfs(self, r : int):
         # todo
         pass
 
+    
     def dfs(self, r : int):
         # todo
         pass
 
+    
     def __str__(self):
         s = ""
         for i in range(0, self.n):
