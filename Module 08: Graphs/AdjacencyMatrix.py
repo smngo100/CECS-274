54474ed299d03e350d1490d5f0b2b9f34db98bd7
smngo100 <smngo100@gmail.com>
2024-12-15 23:14:57 -0800

    Update AdjacencyMatrix.py

--- Module 08: Graphs/AdjacencyMatrix.py
+++ Module 08: Graphs/AdjacencyMatrix.py
@@ -1,53 +1,81 @@
 from Interfaces import Graph, List
 import numpy as np
+import ArrayStack
 """An implementation of the adjacency list representation of a graph"""
 
 class AdjacencyMatrix(Graph):
 
     def __init__(self, n : int):
         self.n = n
-        self.adj = np.zeros(n*n)
+        self.adj = np.zeros([n,n])
 
-    
     def add_edge(self, i : int, j : int):
-        a[i][j] = True    # add the edge (i, j) to E   
+        if 0 <= i and j < self.n:
+            self.adj[i][j] = 1
 
-    
     def remove_edge(self, i : int, j : int):
-        if a[i][j] == 1: 
-            a[i][j] = 0
-            return True 
+        if self.adj[i][j] == 1:
+            self.adj[i][j] = 0
+            return True
         return False
-        #a[i][j] = False    # remove the edge (i, j) from E 
-    
 
     def has_edge(self, i : int, j: int) ->bool:
-        return a[i][j]    # returns true if the edje (i, j) exists in E 
+        return self.adj[i][j] == 1
 
-    
     def out_edges(self, i) -> List:
-        # todo        # returns a list of all integers j such that (i, j) ∈ E
-        for i in j:
-            if a[i][j] == 1: 
-                edges.append[j]
-        return edges 
+        edges = []
+        for j in range(len(self.adj[i])):   # for every entry at column j in row i
+            if self.adj[i][j] == 1:
+                edges.append(j)
+        return edges
 
-    
-    def in_edges(self, i) -> List:
-        # todo         # returns a list of all integers i such that (i, j) ∈ E
-        pass
+    def in_edges(self, j) -> List:
+        edges = []
+        for i in range(len(self.adj)):   # for every row i at column j
+            if self.adj[i][j] == 1:
+                edges.append(i)
+        return edges
 
-    
     def bfs(self, r : int):
-        # todo
-        pass
+        traversal = []  # empty list traversal
+        seen = [False] * self.n   # empty list seen
+        q = []  # empty queue to keep track of the vertices for which we must visit neighbors
 
-    
+        # Visit vertex r
+        q.append(r)
+        traversal.append(r)
+        seen[r] = True
+
+        while q:
+            current = q.pop(0)
+            neighbors = self.out_edges(current)
+            for jk in neighbors:
+                if seen[jk] == False:
+                    # Visit vertex jk
+                    q.append(jk)
+                    traversal.append(jk)
+                    seen[jk] = True
+        return traversal
+
     def dfs(self, r : int):
-        # todo
-        pass
+        traversal = []  # empty list traversal
+        s = ArrayStack.ArrayStack()
+        seen = [False] * self.n
+        s.push(r)
+        while s.size() > 0:
+            current = s.pop()
+            if seen[current] == False:
+                traversal.append(current)
+                seen[current] = True
+            neighbors = self.out_edges(current)
+            for neighbor in reversed(neighbors):
+                if seen[neighbor] == False:
+                    s.push(neighbor)
+        return traversal
 
-    
+    def size(self):
+        return self.n
+
     def __str__(self):
         s = ""
         for i in range(0, self.n):
